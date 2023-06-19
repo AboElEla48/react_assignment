22db98a9ce83b93bfcd1fd5fc31f4a8a0e83b6dc
Ahmed AboElEla <eng.a.aboelela@gmail.com>
2023-06-19 13:25:06 +0300

    Add user components

--- my-assignment/src/App.js
+++ my-assignment/src/App.js
@@ -1,23 +1,18 @@
 import logo from './logo.svg';
 import './App.css';
 
+import UserInput from './UserInput/UserInput.js';
+import UserOutput from './UserOutput/UserOutput.js';
+
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+      <UserInput />
+      <UserOutput userName ='Hamada'/>
+      <UserOutput userName ='Hamada'/>
+      <UserOutput userName ='Hamada'/>
+
     </div>
   );
 }
---
+++ my-assignment/src/UserOutput/UserOutput.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const userOutput = (props) => {
+
+  return (
+    <div>
+      <p>Username: {props.userName}</p>
+      <p>I hope i will overwritten!</p>
+    </div>
+  );
+
+};
+
+export default userOutput;
